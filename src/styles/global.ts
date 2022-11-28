8d8ecb4de1314af1583452554dee03e68f66f39e
RaulSCoelho <raul.s.coelho@gmail.com>
2022-11-28 00:28:45 -0300

    global css

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,18 +1,21 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
-  *{
-    margin: 0;
+  html, body {
     padding: 0;
-    outline: none !important;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  * {
     box-sizing: border-box;
   }
 
-  body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-    sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
+  a {
+    color: inherit;
+    text-decoration: none;
   }
 `
